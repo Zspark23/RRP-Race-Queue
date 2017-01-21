86942aa8b011d057b9753dc43437036fb1065817
Zack <spicer.zack8@gmail.com>
2017-01-21 17:07:47 -0500

    Fixed bug causing crashes

--- RRP Race Queue/AppDelegate.swift
+++ RRP Race Queue/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let notificationTypes: UIUserNotificationType = [UIUserNotificationType.alert, .badge, .sound]
         let pushNotificationSettings = UIUserNotificationSettings(types: notificationTypes, categories: nil)
@@ -23,32 +23,58 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-        DispatchQueue.main.async(execute: { ()
-            let postBody = String(format: "{\"type\":\"iOS\",\"token\":\"%@\",\"userID\":\"\"}", token)
-            let endBody = URL(string: "http://db.racereplay.co:8080/device/")
-            var request = URLRequest(url: endBody!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 30.0)
+        
+        let parameters = ["token": token,
+                          "platform": "ios"]
+        
+        let url = URL(string: "http://54.174.196.84/device/register")!
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
             
-            request.httpMethod = "POST"
-            request.httpBody = postBody.data(using: String.Encoding.utf8)
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            guard error == nil else {
+                return
+            }
             
-            //let session = URLSession()
-            //let task = session.uploadTask(with: request, from: postBody.data(using: .utf8)!)
-            //print(postBody)
-            //print(postBody.data(using: .utf8)!)
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
         })
+        task.resume()
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue:"load"), object: nil)
-        //let alert = userInfo["aps"]! as! [String : Any]
-        //print(alert["alert"]!)
+        let counter = userInfo.count
+        print(userInfo)
+        let alert = userInfo["aps"]! as! [String : Any]
+        print(counter)
+        print(alert)
     }
-
-
+    
+    
 }
 
